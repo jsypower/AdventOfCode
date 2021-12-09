83e6faea19db2cf1b08d9210d72fbbaa89134f0f
jsypower <jordan@defencelogic.io>
2021-12-09 17:02:20 +0000

    Shortened Day 1

--- 2021/Day-1/day1.py
+++ 2021/Day-1/day1.py
@@ -3,13 +3,10 @@
 with open('input.txt', 'r') as f:
 	input = f.readlines()
 
-vals = iter(input)
 prev = int(input[0])
-next(vals)
-
 answer = 0
 
-for val in vals:
+for val in input[1:]:
 	if int(val) > prev:
 		answer += 1
 	prev = int(val)
